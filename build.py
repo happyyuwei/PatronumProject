cbc065a0d4281a9745b8cbb7c40da97344e91f4e
happyyuwei1994 <happyyuwei1994@qq.com>
2020-07-30 11:11:12 +0800

    添加mvnw权限

--- build.py
+++ build.py
@@ -42,7 +42,7 @@ print("Spring boot backend source code pull.")
 
 # 2. 构建后端
 # 添加mvnm权限
-os.chmod("./mvnw", stat.S_IXOTH)
+os.system("chmod ./mvnw 777")
 # 使用mvnw构建后端
 child = subprocess.Popen("./mvnw clean package", shell=True)
 child.wait()
